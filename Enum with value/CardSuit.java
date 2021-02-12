ff14bf14f510db5e266b687c42dc3fc8263ae683
jasper <jasperzhou1129@gmail.com>
2021-02-12 17:48:11 -0500

    enum

---
+++ Enum with value/CardSuit.java
@@ -0,0 +1,11 @@
+public enum CardSuit {
+    Spade(1),
+    Diamond(2),
+    Heart(3),
+    Club(4);
+    int value;
+    CardSuit(int value) {
+        this.value = value;
+    }
+
+}
---
+++ Enum with value/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(CardSuit.Diamond.value);
+        System.out.println(CardSuit.Club.value);
+        System.out.println(CardSuit.Spade.value);
+        System.out.println(CardSuit.Heart.value);
+
+    }
+}
