7e40f1ece2ad18ebbce7589b4eaea22eb657bb9b
jasper <jasperzhou1129@gmail.com>
2021-06-09 17:15:46 -0400

    serialization

---
+++ Serializable/src/A.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class A implements Serializable {
+    int i;
+    String s;
+
+    public A(int i, String s) {
+        this.i = i;
+        this.s = s;
+    }
+}
