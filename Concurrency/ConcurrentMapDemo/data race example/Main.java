007ddfaf63e083c4f5e06e4794f926315a2bf0f6
jasper <jasperzhou1129@gmail.com>
2021-06-26 13:41:31 -0400

    data race

---
+++ Concurrency/ConcurrentMapDemo/data race example/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static List<Integer> parallelSum(Map<String, Integer> map, int executionTimes) throws InterruptedException {
+        List<Integer> sumList = new ArrayList<>(1000);
+        for (int i = 0; i < executionTimes; i++) {
+            map.put("test", 0);
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
+            for (int j = 0; j < 10; j++) {
+                executorService.execute(() -> {
+                    for (int k = 0; k < 10; k++) {
+                        map.computeIfPresent("test", (key, value) -> value + 1);
+                    }
+                });
+            }
+            executorService.shutdown();
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+            sumList.add(map.get("test"));
+        }
+
+        return sumList;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> sumList = parallelSum(map, 100);
+        System.out.println(sumList);
+    }
+}
